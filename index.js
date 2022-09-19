a7577b9ff5f5066bd4a80da8673dde1c74fe0bde
+ <+>
2022-09-19 17:21:13 -0400

    commit tree 9088089dd139a1bb72d3f46a34db371ee55f12d2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+ function superbowlWin(record){
+
+    const victory = record.find( object  =>  object.result ==="W");
+    console.log (victory)
+    //console.log (victory.year)
+    if(victory === undefined){
+        return victory
+    }
+    return (victory.year)
+
+ }
